9e4445ffe67d3d234cf4272bc03e73f8e847d71a
Arand2411 <106779928+Arand2411@users.noreply.github.com>
2023-03-28 10:54:55 -0600

    Array de strings de marcas

--- CS_G4_Pfinal/src/cs_g4_pfinal/Principal.java
+++ CS_G4_Pfinal/src/cs_g4_pfinal/Principal.java
@@ -1,5 +1,8 @@
 package cs_g4_pfinal;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 
 public class Principal {
 
@@ -8,4 +11,24 @@ public class Principal {
         
     }
     
+    public void ArrayListaMarcas(){
+        ArrayList<String> lista = new ArrayList<String>(); // Creamos el ArrayList
+
+        String input = JOptionPane.showInputDialog("Ingrese un string o la palabra'salir' para terminar:");
+
+        // Iteramos hasta que el usuario escriba 'salir'
+        while (!input.equalsIgnoreCase("salir")) {
+            lista.add(input); // Agregamos el string al ArrayList
+            input = JOptionPane.showInputDialog("Ingrese otro string o la palabra 'salir' para terminar:");
+        }
+
+        // Creamos un mensaje que muestra el contenido del ArrayList
+        String mensaje = "Contenido del ArrayList:\n";
+        for (String str : lista) {
+            mensaje += str + "\n";
+        }
+
+        // Mostramos el contenido del ArrayList en un cuadro de diálogo
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
 }
