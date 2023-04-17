700f8b94ee3f05af30e7c6431fa3ea2ac37860a6
Pyro <oreinavallecillo@gmail.com>
2023-04-17 15:26:39 -0600

    Metodos de busqueda/filtrado hecho Oscar

--- CS_G4_Pfinal/src/cs_g4_pfinal/Listas.java
+++ CS_G4_Pfinal/src/cs_g4_pfinal/Listas.java
@@ -1,6 +1,7 @@
 package cs_g4_pfinal;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import javax.swing.JOptionPane;
 
 public class Listas {
@@ -18,7 +19,7 @@ public class Listas {
         JOptionPane.showMessageDialog(null, "Lista de Marcas: \n" + Mensaje);
     }
 
-    public void AgregarListaProductos(ArrayList listaNoP, ArrayList marcas) {
+    public void AgregarListaProductos(ArrayList listaProductos, ArrayList marcas) {
         int op = 0;
         while (op != 3) {
             op = Integer.parseInt(JOptionPane.showInputDialog(null, "El producto es?\n"
@@ -39,7 +40,7 @@ public class Listas {
                     }
                     String Marca = JOptionPane.showInputDialog("Lista de Marcas: \n" + Mensaje + "\nDigite la marca de la lista que quiere escoger");
 
-                    listaNoP.add(new NoPerecederos(true, Marca, id, Cantidad, Precio, Nombre));
+                    listaProductos.add(new NoPerecederos(true, Marca, id, Cantidad, Precio, Nombre));
                     break;
                 case 2:
                     Nombre = JOptionPane.showInputDialog(null, "Digite el nombre del producto:");
@@ -53,7 +54,7 @@ public class Listas {
                     }
                     Marca = JOptionPane.showInputDialog("Lista de Marcas: \n" + Mensaje + "\nDigite la marca de la lista que quiere escoger");
 
-                    listaNoP.add(new Perecederos(true, Marca, id, Cantidad, Precio, Nombre));
+                    listaProductos.add(new Perecederos(true, Marca, id, Cantidad, Precio, Nombre));
                     break;
                 case 3:
                     break;
@@ -62,11 +63,83 @@ public class Listas {
             }
         }
     }
-    public void MostrarListaProductos(ArrayList listaNoP) {
+
+    public void MostrarListaProductos(ArrayList ListaProductos) {
         String Mensaje = "";
-        for (Object f : listaNoP) {
+        for (Object f : ListaProductos) {
             Mensaje = Mensaje + f + "\n";
         }
         JOptionPane.showMessageDialog(null, "Lista de Productos: \n" + Mensaje);
+    }
+
+    public void BuscarID(ArrayList ListaProductos, int IDbuscado) {
+        Iterator<Producto> itListaProductos = ListaProductos.iterator();
+        boolean validar = false;
+        while (itListaProductos.hasNext() && validar == false) {
+            Producto prod = itListaProductos.next();
+            if (prod.getId() == IDbuscado) {
+                JOptionPane.showMessageDialog(null, "ID encontrado \n Producto:\n  " + prod.toString());
+                validar = true;
+            }
+        }
+        if (validar == false) {
+            JOptionPane.showMessageDialog(null, "Id no encontrado");
+        }
+
+    }
+
+    public void BuscarMarca(ArrayList ListaProductos, ArrayList Marcas) {
+        String Mensaje = "";
+        for (Object f : Marcas) {
+            Mensaje = Mensaje + f + "\n";
+        }
+        String MarcaBuscada = JOptionPane.showInputDialog("Lista de Marcas: \n" + Mensaje + "\nDigite la marca de la lista que quiere buscar:");
+        Iterator<MarcadeProducto> itListaProductos = ListaProductos.iterator();
+        boolean validar = false;
+
+        while (itListaProductos.hasNext() && validar == false) {
+            MarcadeProducto productos = itListaProductos.next();
+            if (productos.getNombreMarca().equals(MarcaBuscada)) {
+                JOptionPane.showMessageDialog(null, "La Marca Coincide:");
+                validar = true;
+            }
+        }
+        if (validar == false) {
+            JOptionPane.showMessageDialog(null, "Marca no encontrada, intetelo de nuevo");
+        } else {
+            String ListaProdMarcas = "";
+            Iterator<MarcadeProducto> itListaMarcas = ListaProductos.iterator();
+            while (itListaMarcas.hasNext()) {
+                MarcadeProducto prod = itListaMarcas.next();
+                if (prod.getNombreMarca().equals(MarcaBuscada)) {
+                    ListaProdMarcas = ListaProdMarcas + prod.toString() + "\n"; 
+                }
+            }
+            JOptionPane.showMessageDialog(null, "Lista  de Productos Filtrada por Marca " + MarcaBuscada + "\n" + ListaProdMarcas);
+        }
+    }
+    
+    public void MostrarNoPerecederos(ArrayList ListaProductos) {
+        String ListaNoPerecederos = "";
+        Iterator<Producto> itNoPerecederos = ListaProductos.iterator();
+        while(itNoPerecederos.hasNext()){
+            Producto NoP = itNoPerecederos.next();
+            if(NoP.Descripcion().equals("No Percederos")){
+                 ListaNoPerecederos = ListaNoPerecederos + NoP.toString() + "\n";
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Lista  de Productos Filtrada por No Perecederos"+"\n" + ListaNoPerecederos);
+    }
+    
+    public void MostrarPerecederos(ArrayList ListaProductos) {
+        String ListaPerecederos = "";
+        Iterator<Producto> itPerecederos = ListaProductos.iterator();
+        while(itPerecederos.hasNext()){
+            Producto NoP = itPerecederos.next();
+            if(NoP.Descripcion().equals("Percederos")){
+                 ListaPerecederos = ListaPerecederos + NoP.toString() + "\n";
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Lista  de Productos Filtrada por Perecederos"+"\n" + ListaPerecederos);
     }
 }
--- CS_G4_Pfinal/src/cs_g4_pfinal/Principal.java
+++ CS_G4_Pfinal/src/cs_g4_pfinal/Principal.java
@@ -13,22 +13,25 @@ public class Principal {
         ArrayList<Producto> listaProductos = new ArrayList<>();
 //        ArrayList<NoPerecederos> listNoP = new ArrayList<>();
 //        ArrayList<Perecederos> listP = new ArrayList<>();
-        
+
         Listas l  = new Listas();
         ///////JFRAME MENU /////////
 //        frm_MenuPrincipal p = new frm_MenuPrincipal();
-        ///////////////////////////
+///////////////////////////
         
         
 //        Menu de Pruebas
         int op = 0;
-         while (op != 5) {
+         while (op != 9) {
             op = Integer.parseInt(JOptionPane.showInputDialog(null, "Menu Principal\n"
                     + "1. Prueba AgregarMarcas \n"
                     + "2. Prueba MostrarMarcas\n"
                     + "3. Prueba AgregarProductos \n"
                     + "4.Prueba MostrarProductos \n"
-                    + "5. Salir\n"
+                    + "5.Prueba BUSCARID \n"
+                    + "6.Prueba BuscarMARCA \n"
+                    + "7.Prueba MostrarNOPerecederos \n"
+                    + "8. Salir\n"
                     +"Digite el dato de la opcion que desea:"));
             switch (op) {
                 case 1:
@@ -44,6 +47,19 @@ public class Principal {
                     l.MostrarListaProductos(listaProductos);
                     break;
                 case 5:
+                    int IDbuscado = Integer.parseInt(JOptionPane.showInputDialog("Digite el id que deseas buscas:"));
+                    l.BuscarID(listaProductos, IDbuscado);
+                    break;
+                case 6:
+                    l.BuscarMarca(listaProductos, marcas);
+                    break;
+                case 7:
+                    l.MostrarNoPerecederos(listaProductos);
+                    break;
+                case 8:
+                    l.MostrarPerecederos(listaProductos);
+                    break;
+                case 9:
                     System.exit(0);
                 default:
                     JOptionPane.showMessageDialog(null, "Error opcion invalida");
