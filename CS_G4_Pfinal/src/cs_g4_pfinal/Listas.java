38b1fd77732826b7540b6af3104afce10ea2cd7a
Pyro <oreinavallecillo@gmail.com>
2023-04-18 09:51:43 -0600

    Metodos Eliminar y Modificar agregados

--- CS_G4_Pfinal/src/cs_g4_pfinal/Listas.java
+++ CS_G4_Pfinal/src/cs_g4_pfinal/Listas.java
@@ -112,34 +112,135 @@ public class Listas {
             while (itListaMarcas.hasNext()) {
                 MarcadeProducto prod = itListaMarcas.next();
                 if (prod.getNombreMarca().equals(MarcaBuscada)) {
-                    ListaProdMarcas = ListaProdMarcas + prod.toString() + "\n"; 
+                    ListaProdMarcas = ListaProdMarcas + prod.toString() + "\n";
                 }
             }
             JOptionPane.showMessageDialog(null, "Lista  de Productos Filtrada por Marca " + MarcaBuscada + "\n" + ListaProdMarcas);
         }
     }
-    
+
     public void MostrarNoPerecederos(ArrayList ListaProductos) {
         String ListaNoPerecederos = "";
         Iterator<Producto> itNoPerecederos = ListaProductos.iterator();
-        while(itNoPerecederos.hasNext()){
+        while (itNoPerecederos.hasNext()) {
             Producto NoP = itNoPerecederos.next();
-            if(NoP.Descripcion().equals("No Percederos")){
-                 ListaNoPerecederos = ListaNoPerecederos + NoP.toString() + "\n";
+            if (NoP.Descripcion().equals("No Percederos")) {
+                ListaNoPerecederos = ListaNoPerecederos + NoP.toString() + "\n";
             }
         }
-        JOptionPane.showMessageDialog(null, "Lista  de Productos Filtrada por No Perecederos"+"\n" + ListaNoPerecederos);
+        JOptionPane.showMessageDialog(null, "Lista  de Productos Filtrada por No Perecederos" + "\n" + ListaNoPerecederos);
     }
-    
+
     public void MostrarPerecederos(ArrayList ListaProductos) {
         String ListaPerecederos = "";
         Iterator<Producto> itPerecederos = ListaProductos.iterator();
-        while(itPerecederos.hasNext()){
+        while (itPerecederos.hasNext()) {
             Producto NoP = itPerecederos.next();
-            if(NoP.Descripcion().equals("Percederos")){
-                 ListaPerecederos = ListaPerecederos + NoP.toString() + "\n";
+            if (NoP.Descripcion().equals("Percederos")) {
+                ListaPerecederos = ListaPerecederos + NoP.toString() + "\n";
             }
         }
-        JOptionPane.showMessageDialog(null, "Lista  de Productos Filtrada por Perecederos"+"\n" + ListaPerecederos);
+        JOptionPane.showMessageDialog(null, "Lista  de Productos Filtrada por Perecederos" + "\n" + ListaPerecederos);
+    }
+
+    public void EliminarMarca(ArrayList Marcas, String MarcaBuscada) {
+        boolean encontrado = false;
+
+        for (Object f : Marcas) {
+            if (f.equals(MarcaBuscada)) {
+                encontrado = true;
+                Marcas.remove(f);
+                JOptionPane.showMessageDialog(null, "Marca ELIMINADA con exito!");
+                break;
+            }
+        }
+        if (encontrado == false) {
+            JOptionPane.showMessageDialog(null, "Marca No encontrada ");
+        }
+
+    }
+
+    public void EliminarProducto(ArrayList ListaProductos, int IDbuscado) {
+
+        Iterator<Producto> itListaProductos = ListaProductos.iterator();
+        boolean validar = false;
+        while (itListaProductos.hasNext() && validar == false) {
+            Producto prod = itListaProductos.next();
+            if (prod.getId() == IDbuscado) {
+                ListaProductos.remove(prod);
+                JOptionPane.showMessageDialog(null, "Producto ELIMINADO con exito!");
+                validar = true;
+            }
+        }
+        if (validar == false) {
+            JOptionPane.showMessageDialog(null, "Id no encontrado");
+        }
+
+    }
+
+    public void ModificarMarca(ArrayList Marcas) {
+        boolean encontrado = false;
+        String MarcaBuscada = "";
+        String Mensaje = "";
+        for (Object f : Marcas) {
+            Mensaje = Mensaje + f + "\n";
+        }
+        MarcaBuscada = JOptionPane.showInputDialog("Lista de Marcas: \n" + Mensaje + "\nDigite la marca de la lista que quiere escoger");
+
+        for (Object f : Marcas) {
+            if (f.equals(MarcaBuscada)) {
+                encontrado = true;
+                Marcas.remove(f);
+                String NuevaMarca= JOptionPane.showInputDialog("Ingrese la  nueva marca:");
+                Marcas.add(NuevaMarca);
+                JOptionPane.showMessageDialog(null, "Marca Editada con exito!");
+                break;
+            }
+        }
+        if (encontrado == false) {
+            JOptionPane.showMessageDialog(null, "Marca No encontrada ");
+        }
+
+    }
+
+    public void ModificarProducto(ArrayList ListaProductos, int IDbuscado, ArrayList marcas) {
+        Iterator<Producto> itListaProductos = ListaProductos.iterator();
+        boolean validar = false;
+        while (itListaProductos.hasNext() && validar == false) {
+            Producto prod = itListaProductos.next();
+            if (prod.getId() == IDbuscado) {
+                ListaProductos.remove(prod);
+
+                String Nombre = JOptionPane.showInputDialog(null, "Digite el nuevo nombre del producto:");
+                int Precio = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite el  nuevo precio del producto:"));
+                int id = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite el nuevo id del producto:"));
+                int Cantidad = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite la nueva cantidad del producto:"));
+                String Mensaje = "";
+                for (Object f : marcas) {
+                    Mensaje = Mensaje + f + "\n";
+                }
+
+                validar = true;
+                String Marca = JOptionPane.showInputDialog("Lista de Marcas: \n" + Mensaje + "\nDigite la marca de la lista que quiere escoger");
+                int Categoria = Integer.parseInt(JOptionPane.showInputDialog(null, "El producto es?\n"
+                        + "1. No Perecederos \n"
+                        + "2. Perecederos\n"
+                        + "3. Cancelar\n"
+                        + "Digite el dato de la opcion que desea:"));
+                if (Categoria == 1) {
+                    ListaProductos.add(new NoPerecederos(true, Marca, id, Cantidad, Precio, Nombre));
+                    JOptionPane.showMessageDialog(null, "Producto Editado con exito!");
+                } else if (Categoria == 2) {
+                    ListaProductos.add(new Perecederos(true, Marca, id, Cantidad, Precio, Nombre));
+                    JOptionPane.showMessageDialog(null, "Producto Editado con exito!");
+                } else if (Categoria == 3) {
+                    break;
+                }
+
+                if (validar == false) {
+                    JOptionPane.showMessageDialog(null, "Id no encontrado");
+                }
+            }
+        }
     }
 }
--- CS_G4_Pfinal/src/cs_g4_pfinal/Principal.java
+++ CS_G4_Pfinal/src/cs_g4_pfinal/Principal.java
@@ -16,55 +16,75 @@ public class Principal {
 
         Listas l  = new Listas();
         ///////JFRAME MENU /////////
-//        frm_MenuPrincipal p = new frm_MenuPrincipal();
+        frm_MenuPrincipal p = new frm_MenuPrincipal();
 ///////////////////////////
         
         
 //        Menu de Pruebas
-        int op = 0;
-         while (op != 9) {
-            op = Integer.parseInt(JOptionPane.showInputDialog(null, "Menu Principal\n"
-                    + "1. Prueba AgregarMarcas \n"
-                    + "2. Prueba MostrarMarcas\n"
-                    + "3. Prueba AgregarProductos \n"
-                    + "4.Prueba MostrarProductos \n"
-                    + "5.Prueba BUSCARID \n"
-                    + "6.Prueba BuscarMARCA \n"
-                    + "7.Prueba MostrarNOPerecederos \n"
-                    + "8. Salir\n"
-                    +"Digite el dato de la opcion que desea:"));
-            switch (op) {
-                case 1:
-                    l.AgregarListaMarcas(marcas);
-                    break;
-                case 2:
-                    l.MostrarListaMarcas(marcas);
-                    break;
-                case 3:
-                    l.AgregarListaProductos(listaProductos, marcas);
-                    break;
-                case 4:
-                    l.MostrarListaProductos(listaProductos);
-                    break;
-                case 5:
-                    int IDbuscado = Integer.parseInt(JOptionPane.showInputDialog("Digite el id que deseas buscas:"));
-                    l.BuscarID(listaProductos, IDbuscado);
-                    break;
-                case 6:
-                    l.BuscarMarca(listaProductos, marcas);
-                    break;
-                case 7:
-                    l.MostrarNoPerecederos(listaProductos);
-                    break;
-                case 8:
-                    l.MostrarPerecederos(listaProductos);
-                    break;
-                case 9:
-                    System.exit(0);
-                default:
-                    JOptionPane.showMessageDialog(null, "Error opcion invalida");
-            } 
-        }
+//        int op = 0;
+//         while (op != 13) {
+//            op = Integer.parseInt(JOptionPane.showInputDialog(null, "Menu Principal\n"
+//                    + "1. Prueba AgregarMarcas \n"
+//                    + "2. Prueba MostrarMarcas\n"
+//                    + "3. Prueba AgregarProductos \n"
+//                    + "4.Prueba MostrarProductos \n"
+//                    + "5.Prueba BUSCARID \n"
+//                    + "6.Prueba BuscarMARCA \n"
+//                    + "7.Prueba MostrarNOPerecederos \n"
+//                    + "8.Prueba MostrarPerecederos \n"
+//                    + "9.Prueba EliminarMarca \n"
+//                    + "10.Prueba EditarMarca \n"
+//                    + "11.Prueba EliminarProducto\n"
+//                    + "12.Prueba EditarProducto \n"
+//                    + "13. Salir\n"
+//                    +"Digite el dato de la opcion que desea:"));
+//            switch (op) {
+//                case 1:
+//                    l.AgregarListaMarcas(marcas);
+//                    break;
+//                case 2:
+//                    l.MostrarListaMarcas(marcas);
+//                    break;
+//                case 3:
+//                    l.AgregarListaProductos(listaProductos, marcas);
+//                    break;
+//                case 4:
+//                    l.MostrarListaProductos(listaProductos);
+//                    break;
+//                case 5:
+//                    int IDbuscado = Integer.parseInt(JOptionPane.showInputDialog("Digite el id que deseas buscar:"));
+//                    l.BuscarID(listaProductos, IDbuscado);
+//                    break;
+//                case 6:
+//                    l.BuscarMarca(listaProductos, marcas);
+//                    break;
+//                case 7:
+//                    l.MostrarNoPerecederos(listaProductos);
+//                    break;
+//                case 8:
+//                    l.MostrarPerecederos(listaProductos);
+//                    break;
+//                case 9:
+//                    String MarcaBuscada = JOptionPane.showInputDialog("Ingrese la marca que desea Eliminar:");
+//                    l.EliminarMarca(marcas, MarcaBuscada);
+//                    break;
+//                case 10:
+//                    l.ModificarMarca(marcas);
+//                    break;
+//                case 11:
+//                    IDbuscado = Integer.parseInt(JOptionPane.showInputDialog("Digite el id del producto que deseas Eliminar:"));
+//                    l.EliminarProducto(listaProductos, IDbuscado);
+//                    break;
+//                case 12:
+//                    IDbuscado = Integer.parseInt(JOptionPane.showInputDialog("Digite el id del producto que deseas Eliminar:"));
+//                    l.ModificarProducto(listaProductos, IDbuscado, marcas);
+//                    break;
+//                case 13:
+//                    System.exit(0);
+//                default:
+//                    JOptionPane.showMessageDialog(null, "Error opcion invalida");
+//            } 
+//        }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////         
     }
     
